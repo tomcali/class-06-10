811791d74aa6a3340809ade2445464b835afdd2e
tomcali <tmiller@toutbay.com>
2017-06-10 08:46:11 -0700

    start class

---
+++ React_19.3/18-IntroMERN/Solved/server.js
@@ -0,0 +1,103 @@
+// Include Server Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Require Click schema
+var Click = require("./models/click");
+
+// Create a new express app
+var app = express();
+// Sets an initial port. We'll use this later in our listener
+var PORT = process.env.PORT || 3000;
+
+// Run Morgan for Logging
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use(express.static("./public"));  // default location for static files like css
+
+// -------------------------------------------------
+
+// MongoDB configuration (Change this URL to your own DB)
+// mongoose.connect("mongodb://admin:codingrocks@ds023674.mlab.com:23674/heroku_5ql1blnl");
+// the directory that we put here will be created in the root location for MongoDB
+mongoose.connect("mongodb://localhost/class-06-08");
+var db = mongoose.connection;
+
+db.on("error", function(err) {
+  console.log("Mongoose Error: ", err);
+});
+
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
+// -------------------------------------------------
+
+// Main "/" Route. This will redirect the user to our rendered React application
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/public/index.html"); // give this file back
+    // all the react app will be placed in index.html
+});
+
+// This is the route we will send GET requests to retrieve our most recent click data.
+// We will call this route the moment our page gets rendered
+// usually it is a client/browser that is making the request
+app.get("/api", function(req, res) {   // api route on the server
+
+  // This GET request will search for the latest click count
+    // client makes request to server to get the click count
+    // and server is making information from MongoDB database and collection
+  Click.find({}).exec(function(err, doc) {  // finding information from the collection Click
+      // this info includes the click count
+
+    if (err) {
+      console.log(err);
+    }
+    else {
+      res.send(doc);  // if no error, it sends back the data from the Click collection
+    }
+  });
+});
+
+// This is the route we will send POST requests to save each click.
+// We will call this route the moment the "click" or "reset" button is pressed.
+// picking up request sending up the number of clicks to be stored in database
+app.post("/api", function(req, res) {
+
+  var clickID = req.body.clickID;
+  var clicks = parseInt(req.body.clicks);
+
+  // Note how this route utilizes the findOneAndUpdate function to update the clickCount
+  // { upsert: true } is an optional object we can pass into the findOneAndUpdate method
+  // If included, Mongoose will create a new document matching the description if one is not found
+    // document in MongoDB like a row in RDMS
+    // if document does not exist... MongoDB will create it
+  Click.findOneAndUpdate({  // find document based on parameters... has clickID = clickID
+    clickID: clickID
+  }, {
+    $set: {
+      clicks: clicks
+    }
+  }, { upsert: true }).exec(function(err) {
+
+    if (err) {
+      console.log(err);
+    }
+    else {
+      res.send("Updated Click Count!");
+    }
+  });
+});
+
+// -------------------------------------------------
+
+// Starting our express server
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
---
+++ z-React_19.3-old/18-IntroMERN/Solved/app/components/Parent.js
@@ -0,0 +1,149 @@
+// Include React
+var React = require("react");
+
+// Here we include all of the sub-components
+var Child = require("./Child");
+
+// Requiring our helper for making API calls
+var helpers = require("../utils/helpers");
+
+// Create the Parent Component
+var Parent = React.createClass({
+
+  // Here we set a generic state associated with the number of clicks
+  getInitialState: function() {
+    return {
+      clicks: 0,
+      clickID: "Main"
+    };
+  },
+
+  //  On load display the number of clicks
+    // executes only on load
+    // a lifecycle event built into React
+  componentDidMount: function() {
+    console.log("COMPONENT MOUNTED");
+
+    // The moment the page renders on page load, we will retrieve the previous click count.
+    // We will then utilize that click count to change the value of the click state.
+      // this is a method that we defined... middleware
+      // this is making an axios.get request
+      // React making request of server via axios
+      // getting info from the api
+      // get all info from the click collection
+      // when you get it back there is a promise
+    helpers.getClicks()
+      .then(function(response) {
+        // Using a ternary operator we can set newClicks to the number of clicks in our response object
+        // If we don't have any clicks in our database, set newClicks to 0
+          // ternary operator
+          // if response.data.length is true then the first statement is the value of newClicks
+          // if false then newClicks is equal to 0
+          // standard ternary conditional operator
+        var newClicks = response.data.length ? response.data[0].clicks : 0;
+
+        // setter built into React... setting state
+        this.setState({
+          clicks: newClicks
+        });
+        console.log("RESULTS", response);
+        console.log("Saved clicks", newClicks);
+
+        // binds the lexical scope to the component
+          // this nested one level deep refers to the object
+      }.bind(this));
+  },
+  // Whenever our component updates, the code inside componentDidUpdate is run
+    // another component lifecycle built into React
+  componentDidUpdate: function(prevState) {
+    console.log("COMPONENT UPDATED");
+
+    // We will check if the click count has changed...
+    if (prevState.clicks !== this.state.clicks) {
+
+      // If it does, then update the clickcount in MongoDB
+        // axios saving info to the server
+      helpers.saveClicks({ clickID: this.state.clickID, clicks: this.state.clicks })
+        .then(function() {
+          console.log("Posted to MongoDB");
+        });
+    }
+  },
+  // Whenever the button is clicked we'll use setState to add to the clickCounter
+  // Note the syntax for setting the state
+    // this refers to the component object
+  handleClick: function() {
+    this.setState({ clicks: this.state.clicks + 1 });
+  },
+
+  // Whenever the button is clicked we'll use setState to reset the clickCounter
+  // This will reset the clicks -- and it will be passed ALL children
+  resetClick: function() {
+    this.setState({ clicks: 0 });
+  },
+
+  // Here we render the function
+  render: function() {
+    return (
+      <div className="container">
+
+        <div className="row">
+
+          <div className="jumbotron">
+            <h2>All my children: A Tale of Reactive Components</h2>
+            <hr />
+            <p>
+              <em>Now backed by the power of MongoDB!</em>
+              Just visit <a href="/api">/api</a> to check out the DB!
+            </p>
+            <p>
+              {/*
+                Here we create a button click.
+                Note how we have an onClick event associate with our handleClick function.
+              */}
+              <button
+                className="btn btn-primary btn-lg"
+                type="button"
+                onClick={this.handleClick}
+              >
+                CLICK ME!!!!
+              </button>
+              {/* Here we create a button click for resetting the clickCounter */}
+              <button
+                className="btn btn-danger btn-lg"
+                type="button"
+                onClick={this.resetClick}
+              >
+                Reset
+              </button>
+            </p>
+          </div>
+          {/* This represents the "Parent" */}
+          <div className="col-md-12">
+
+            <div className="panel panel-default">
+              <div className="panel-heading">
+                <h3 className="panel-title text-center">Parent</h3>
+              </div>
+              <div className="panel-body text-center">
+
+                {/* This is where we'll show the click count for the parent */}
+                <h1>{this.state.clicks}</h1>
+
+                {/*
+                  Here we'll deploy the child component.
+                  We'll pass it the parent's click counter as a "state"
+                */}
+                <Child clicks={this.state.clicks} />
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+// Export the component back for use in other files
+module.exports = Parent;
